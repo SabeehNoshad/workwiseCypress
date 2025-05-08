6a8b4b8b3e490e627d9c9ba99d3764108e2f0e78
Sabeeh Noshad <sabeehnoshad@gmail..com>
2025-05-08 13:13:54 +0500

    security test api file complete 8/5/25

--- cypress/e2e/https/securityTest.cy.js
+++ cypress/e2e/https/securityTest.cy.js
@@ -43,118 +43,118 @@ describe('security test', () => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Custom Approval Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/CustomApproval/GetAllCustomApproval',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Docs & Archive Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Document/GetAllDocumentList',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Projects Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Project/GetAllProject',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Tasks Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/UserTask/GetAllUserTask',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Contract Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/ContractPurpose/GetAllContract',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Loan Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Loan/GetAllLoan',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Salary Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/EmployeeSalary/GetAllEmployeeSalary',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Leaves Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Leave/GetAllLeave',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Payrole Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Payroll/GetAllPayroll',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Requisition Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Requisition/GetAllRequisition',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For VoucherList Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/Transaction/GetAllTransaction',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For ChartsOfAccount Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/ChartOfAccount/GetAllChartOfAccount',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
         });
       });
-      it('should not leak data in preflight (OPTIONS) request For Expense Module', () => {
+      it('should not leak data in preflight (OPTIONS) request For Assets List Module', () => {
         cy.request({
           method: 'OPTIONS',
-          url: 'https://workw.com/workwapi/api/Expense/GetAllExpense',
+          url: 'https://workw.com/workwapi/api/InventoryItem/GetAllItem',
           failOnStatusCode: false,
         }).then((res) => {
           expect(res.status).to.not.eq(200); // Ideally, should NOT return 200 with sensitive headers
