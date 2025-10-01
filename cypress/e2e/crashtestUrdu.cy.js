fc842a7d24285c787642ad02e7037d80412000a7
Sabeeh Noshad <sabeehnoshad@gmail..com>
2025-10-01 15:09:21 +0500

    crash test for the urdu and german language added

--- cypress/e2e/crashtestUrdu.cy.js
+++ cypress/e2e/crashtestUrdu.cy.js
@@ -1,14 +1,43 @@
-describe("crashtest for the modules against each URL for the Language Urdu",()=>{
-    it('should display the contents against each URL', () => {
+const { login } = require("../helper/login");
+const {routing} = require('../helper/routing')
+
+describe("crashtest for the modules against each URL for the Language choosed",()=>{
+    it('should display the contents against each URL in Urdu', () => {
            login(Cypress.env('adminemail'),Cypress.env('adminpassword'));
         cy.get('.userDetails__body > .name',{timeout:10000}).should('be.visible');
+        cy.get('.userDetails__footer > img').click();
+        cy.get('.dropDown > :nth-child(2) > .ant-collapse-item > .ant-collapse-header > .ant-collapse-expand-icon').click();
+        //selecting urdu
+        cy.get('.ant-collapse-content-box > .list > :nth-child(2)').click()
+
+
 
         routing({
             route:['https://www.workw.com/schedules?f=cal','https://www.workw.com/messenger','https://www.workw.com/mail/INBOX'],
             selectors: [
                 '.fc', // Selector for the schedules page
                 '.HeadName', // Selector for the messenger page
-                '.bg-blue-800' // Selector for the mail inbox page
+                // '.bg-blue-800' // Selector for the mail inbox page
+            ]
+        
+    });
+})
+it('should display the content in German against each url',()=>{
+            login(Cypress.env('adminemail'),Cypress.env('adminpassword'));
+        cy.get('.userDetails__body > .name',{timeout:10000}).should('be.visible');
+        cy.get('.userDetails__footer > img').click();
+        cy.get('.dropDown > :nth-child(2) > .ant-collapse-item > .ant-collapse-header > .ant-collapse-expand-icon').click();
+        //selecting German
+        cy.get('.ant-collapse-content-box > .list > :nth-child(2)').click()
+
+
+
+        routing({
+            route:['https://www.workw.com/schedules?f=cal','https://www.workw.com/messenger','https://www.workw.com/mail/INBOX'],
+            selectors: [
+                '.fc', // Selector for the schedules page
+                '.HeadName', // Selector for the messenger page
+                // '.bg-blue-800' // Selector for the mail inbox page
             ]
         
     });
