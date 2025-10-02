c02d78bef7aacafed8c4d60d5b9b40ef32a048ff
Sabeeh Noshad <sabeehnoshad@gmail..com>
2025-10-02 14:28:22 +0500

    fixex employee updation

--- cypress/e2e/userUpdationlogOut.cy.js
+++ cypress/e2e/userUpdationlogOut.cy.js
@@ -15,15 +15,34 @@ describe('User updation',()=>{
         })
 })
 
-    it('should update own user sucessfully',()=>{
+    it.only('should update own user sucessfully',()=>{
         login(Cypress.env('adminemail'),Cypress.env('adminpassword'))
          cy.get('.userDetails__body > .name',{timeout:10000}).should('be.visible'); 
          cy.visit('https://www.workw.com/employees');
          cy.get('.topBar__buttons > .active').should('be.visible')
          cy.get('.ant-input').type("Work").should('be.visible')
-         cy.get('.sc-hsaIUA').contains("WOrk Test")
+        cy.get('.sc-dVdSBb > :nth-child(1)').contains("Work Test")
          cy.get('.jCKWYE').click();
-         cy.get(URL).should('include',"basicinfo")
+        cy.url().should('include', 'basicInfo');
+    })
+
+     it.only('should update own user sucessfully with sucessfull alert and automatic logout',()=>{
+        login(Cypress.env('adminemail'),Cypress.env('adminpassword'))
+         cy.get('.userDetails__body > .name',{timeout:10000}).should('be.visible'); 
+         cy.visit('https://www.workw.com/employees');
+         cy.get('.topBar__buttons > .active').should('be.visible')
+         cy.get('.ant-input').type("Work").should('be.visible')
+        cy.get('.sc-dVdSBb > :nth-child(1)').contains("Work Test")
+         cy.get('.jCKWYE').first().click();
+        cy.url().should('include', 'basicInfo');
+        cy.get(':nth-child(26) > .ant-row > .ant-form-item-control > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-select > .ant-select-selector > .ant-select-selection-wrap > .ant-select-selection-item').click()
+        cy.get(':nth-child(29) > .ant-row > .ant-form-item-control > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-select > .ant-select-selector > .ant-select-selection-wrap > .ant-select-selection-overflow')
+        .scrollIntoView()
+        .should('be.visible')
+        .click()
+        cy.get('.editButtons > .ant-btn').should('be.visible').scrollIntoView().click();
+         cy.get('.submit-wrapper > .ant-btn').click();
+       cy.get('.ant-notification-notice').should('include','Successfully');
 
     })
 })
